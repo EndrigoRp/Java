067a01b7c89b437ec266f2d68db67733a40e2b20
EndrigoRp <146990716+EndrigoRp@users.noreply.github.com>
2024-01-04 11:38:11 -0300

    Arquivos add iniciais

---
+++ Screen-oo/2887-java-screenmatch-oo-aula02/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,47 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private int epPorTemporada;
+    private boolean ativar;
+    private int minutosPorEp;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpPorTemporada() {
+        return epPorTemporada;
+    }
+
+    public void setEpPorTemporada(int epPorTemporada) {
+        this.epPorTemporada = epPorTemporada;
+    }
+
+    public boolean isAtivar() {
+        return ativar;
+    }
+
+    public void setAtivar(boolean ativar) {
+        this.ativar = ativar;
+    }
+
+    public int getMinutosPorEp() {
+        return minutosPorEp;
+    }
+
+    public void setMinutosPorEp(int minutosPorEp) {
+        this.minutosPorEp = minutosPorEp;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos(){
+        return temporadas * epPorTemporada * minutosPorEp;
+    }
+
+
+}
