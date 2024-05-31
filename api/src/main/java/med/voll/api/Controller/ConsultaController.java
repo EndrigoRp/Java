49a9a269b0a54f880ac6e459051096add31802d9
EndrigoRp <146990716+EndrigoRp@users.noreply.github.com>
2024-05-30 21:37:03 -0300

    'aula30/05'

--- api/src/main/java/med/voll/api/Controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/Controller/ConsultaController.java
@@ -3,15 +3,13 @@ package med.voll.api.Controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
@@ -27,4 +25,10 @@ public class ConsultaController {
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+                agenda.cancelar(dados);
+                return ResponseEntity.noContent().build();
+    }
 }
